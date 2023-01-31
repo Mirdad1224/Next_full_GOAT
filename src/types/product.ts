d1f44924d5f402599cd5b27e1babf92dd21aba4b
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-31 11:54:43 +0330

    create product type

---
+++ src/types/product.ts
@@ -0,0 +1,80 @@
+import { z } from 'zod'
+
+import { category, section, subSection } from '@/data/navdata'
+
+const common = {
+  brand: z.string().min(3, 'Title must be 3 characters long'),
+  title: z.string().min(3, 'Title must be 3 characters long'),
+  metaDescription: z.string().min(5, 'Description must be 5 characters long'),
+  oldPrice: z
+    .number({ invalid_type_error: 'Please enter valid number' })
+    .min(0.01, 'Price must be grateter than $0.01'),
+  newPrice: z
+    .number({ invalid_type_error: 'Please enter valid number' })
+    .min(0.01, 'Price must be grateter than $0.01'),
+  countInStock: z
+    .number({ invalid_type_error: 'Please enter valid number' })
+    .min(1, 'Quantity must be minimum of 1'),
+  category: z
+    .string({ invalid_type_error: 'Please select valid category' })
+    .refine((val) => category.map((c) => c).includes(val as any), {
+      message: 'Please select valid category',
+    }),
+  section: z
+    .string({ invalid_type_error: 'Please select valid section' })
+    .refine((val) => section.map((c) => c).includes(val as any), {
+      message: 'Please select valid section',
+    }),
+  subSection: z
+    .string({ invalid_type_error: 'Please select valid subsection' })
+    .refine((val) => subSection.map((c) => c).includes(val as any), {
+      message: 'Please select valid subsection',
+    }),
+}
+export const productByIdSchema = z.object({
+  id: z.string(),
+})
+export const productUpdateSchema = z.object({
+  description: z.string(),
+  images: z.string().url().array(),
+  sold: z.number().default(0),
+  rating: z.number().default(0),
+  id: z.string(),
+  slug: z.string(),
+  ...common,
+})
+
+export const productFormSchema = z.object({ ...common })
+export const productSchema = z.object({
+  description: z.string(),
+  images: z.string().url().array(),
+  sold: z.number().default(0),
+  rating: z.number().default(0),
+  ...common,
+})
+export type ProductFormSchemaType = z.infer<typeof productFormSchema>
+
+export const getProductSchema = z.object({
+  mostSold: z.boolean().optional(),
+  limit: z.number().optional(),
+  cursor: z.string().nullish(),
+  category: z
+    .string({ invalid_type_error: 'Please select valid category' })
+    .refine((val) => category.map((c) => c).includes(val as any), {
+      message: 'Please select valid category',
+    })
+    .optional(),
+  section: z
+    .string({ invalid_type_error: 'Please select valid section' })
+    .refine((val) => section.map((c) => c).includes(val as any), {
+      message: 'Please select valid section',
+    })
+    .optional(),
+  subSection: z
+    .string({ invalid_type_error: 'Please select valid subsection' })
+    .refine((val) => subSection.map((c) => c).includes(val as any), {
+      message: 'Please select valid subsection',
+    })
+    .array()
+    .optional(),
+})
