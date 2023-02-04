6fefaf1c421ca4e81b9206429fe3f465602afe88
Mirdad1224 <ali.mirdad@yahoo.com>
2023-02-04 11:11:51 +0330

    create category index page

---
+++ src/pages/[category]/index.tsx
@@ -0,0 +1,77 @@
+import { createProxySSGHelpers } from '@trpc/react-query/ssg'
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from 'next'
+import superjson from 'superjson'
+
+import { Meta } from '@/components/common/Meta'
+import { ProductCategory } from '@/components/product/ProductCategory'
+import type { Category } from '@/data/navdata'
+import { kidsCategory, menCategory, womenCategory } from '@/data/navdata'
+import { createContext } from '@/server/context'
+import { appRouter } from '@/server/router/_app'
+
+export default function CategoryPage({
+  category,
+  categoryList,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!categoryList) {
+    return null
+  }
+  return (
+    <>
+      <Meta pageTitle={category} />
+      <ProductCategory
+        category={category as Category}
+        categoryList={categoryList}
+      />
+    </>
+  )
+}
+
+export async function getServerSideProps(
+  ctx: GetServerSidePropsContext<{ category: string }>
+) {
+  const { req, res, params } = ctx
+  const category = params?.category || ''
+  if (category !== 'men' && category !== 'women' && category !== 'kids') {
+    return {
+      notFound: true,
+    }
+  }
+
+  function getCategory() {
+    if (category === 'men') {
+      return menCategory
+    }
+    if (category === 'women') {
+      return womenCategory
+    }
+    if (category === 'kids') {
+      return kidsCategory
+    }
+  }
+
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContext({ req, res } as any),
+    transformer: superjson,
+  })
+  await ssg.product.getAllProduct.fetch({
+    category,
+    subSection: [],
+  })
+  await ssg.product.getAllProduct.fetchInfinite({
+    category,
+    subSection: [],
+  })
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      categoryList: getCategory(),
+      category,
+    },
+  }
+}
